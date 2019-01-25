c0c04da17e02dd6601576d2345ff5a5eb8311ecc
majkel <michal.sobotka.84@gmail.com>
2019-01-25 23:29:15 +0100

    CLASS PERSON - reviev correction

--- Person.cpp
+++ Person.cpp
@@ -3,7 +3,7 @@
 
 Person::Person(const std::string firstName,
                 const std::string lastName,
-                const int64_t personalID,
+                const unsigned long personalID,
                 const bool gender,     
                 const std::string adress)
     : firstName_(firstName),
@@ -18,27 +18,37 @@ std::string Person::getLastName() const
     return lastName_;
 }
 
-int32_t Person::getPersonalID()const
+unsigned long Person::getPersonalID()const
 {
     return personalID_;
 }
 
 Employee::Employee(const std::string firstName,
                 const std::string lastName,
-                const int64_t personalID,
+                const unsigned long personalID,
                 const bool gender,     
                 const std::string adress,
-                const int salaries)
+                const double salary)
     : Person(firstName, lastName, personalID, gender, adress),
-    salaries_(salaries)
+    salary_(salary)
 {}
+
+double Employee::getSalary() const
+{
+    return salary_;
+}
 
 Student::Student(const std::string firstName,
                 const std::string lastName,
-                const int64_t personalID,
+                const unsigned long personalID,
                 const bool gender,     
                 const std::string adress,
-                const int studentID)
+                const unsigned long studentIndex)
     : Person(firstName, lastName, personalID, gender, adress),
-    studentID_(studentID)
+    studentIndex_(studentIndex)
 {}
+
+unsigned long Student::getStudentIndex() const
+{
+    return studentIndex_;
+}
--- Person.hpp
+++ Person.hpp
@@ -9,53 +9,49 @@ class Person
     protected:
         std::string firstName_;
         std::string lastName_;
-        int64_t personalID_;
+        unsigned long personalID_;
         bool gender_;
         std::string adress_;
     
     public:
         Person(const std::string firstName,
                 const std::string lastName,
-                const int64_t personalID,
+                const unsigned long personalID,
                 const bool gender,     
                 const std::string adress);
 
         virtual ~Person() {};
-        virtual int getData() const
-        {
-            return personalID_;
-        }
         std::string getLastName() const;
-        int32_t getPersonalID() const;
+        unsigned long getPersonalID() const;
 };
 
 class Employee : public Person
 { 
-        int salaries_;
+    private:
+        double salary_;
     public:
         Employee(const std::string firstName,
                 const std::string lastName,
-                const int64_t personalID,
+                const unsigned long personalID,
                 const bool gender,     
                 const std::string adress,
-                const int salaries);
+                const double salary);
+
+        double getSalary() const;
 };
 
 class Student : public Person
 {
     private:
-        int32_t studentID_;
+        unsigned long studentIndex_;
         
     public:
         Student(const std::string firstName,
                 const std::string lastName,
-                const int64_t personalID,
+                const unsigned long personalID,
                 const bool gender,     
                 const std::string adress,
-                const int studentID);
+                const unsigned long studentIndex);
 
-        int getData() const
-        {
-            return studentID_;
-        };
+        unsigned long getStudentIndex() const;
 };
